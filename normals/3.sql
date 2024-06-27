8c75c7418bc51cc981bee49c7c640333e9b6e3f8
nestortada <154456349+nestortada@users.noreply.github.com>
2024-06-26 19:53:37 -0500

    Add files via upload

---
+++ normals/3.sql
@@ -0,0 +1,2 @@
+SELECT "0m","100m","200m" FROM "normals"
+WHERE "latitude" =  89.5 AND "longitude" = 179.5
---
+++ normals/6.sql
@@ -0,0 +1,5 @@
+SELECT  "latitude" , "longitude" ,"50m"
+FROM "normals"
+WHERE "latitude" BETWEEN 0 AND 20
+AND "longitude" BETWEEN 55 AND 75;
+
